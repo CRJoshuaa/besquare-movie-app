7e8bb7e490982625b6f2248073235f08fde0bf8e
CRJoshuaa <59153476+CRJoshuaa@users.noreply.github.com>
2021-10-18 15:02:12 +0800

    Branch joshuaa (#5)
    * Remove index.css
    
    * added categories.js
    
    * Added Categories.css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import HomeScreen from "./pages/HomeScreen";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <HomeScreen />
+    </div>
+  );
 }
 
 export default App;
