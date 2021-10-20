04e23e6643d5ff0066d364fd1b57ddf739cbaf35
CRJoshuaa <59153476+CRJoshuaa@users.noreply.github.com>
2021-10-20 11:31:45 +0800

    Branch joshuaa (#11)
    * Remove index.css
    
    * added categories.js
    
    * Added Categories.css
    
    * Added requests.js
    
    * Categories v1
    
    * Category v1.1
    
    * Category v1.3
    
    * Categories v1.4

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Categories from "../components/Categories";
 import FeatureMovie from "../components/FeatureMovie";
 import Header from "../components/Header";
 import "./HomeScreen.css";
@@ -7,9 +8,9 @@ import "../App.css";
 function HomeScreen() {
   return (
     <div>
-      <Categories />
       <Header />
       <FeatureMovie />
+      <Categories />
     </div>
   );
 }
