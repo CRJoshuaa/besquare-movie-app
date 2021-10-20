1ff0204590422f8b2903b2790832e5d14e24114b
Thisyahlen Nair <thisyahlen@besquare.com.my>
2021-10-20 16:35:03 +0800

    movie page damage control

--- src/components/CategoryPoster.js
+++ src/components/CategoryPoster.js
@@ -6,18 +6,20 @@ function CategoryPoster(props) {
   const base_URL = "https://image.tmdb.org/t/p/original/";
 
   return (
-    <img
-      className="category-poster"
-      src={`${base_URL}${
-        props.movieObj.poster_path || props.movieObj.backdrop_path
-          ? props.movieObj.poster_path
-          : props.movieObj.backdrop_path
-      }`}
-      alt=""
-      onClick={() => {
-        console.log(props.movieObj.popularity);
-      }}
-    />
+    <Link to={`movie/${props.movieObj.id}`}>
+      <img
+        className="category-poster"
+        src={`${base_URL}${
+          props.movieObj.poster_path || props.movieObj.backdrop_path
+            ? props.movieObj.poster_path
+            : props.movieObj.backdrop_path
+        }`}
+        alt=""
+        onClick={() => {
+          console.log(props.movieObj.popularity);
+        }}
+      />
+    </Link>
   );
 }
 
--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -4,7 +4,6 @@ import FeatureMovie from "../components/FeatureMovie";
 import Header from "../components/Header";
 import "./HomeScreen.css";
 import "../App.css";
-import MoviePage from "./MoviePage";
 
 function HomeScreen() {
   return (
