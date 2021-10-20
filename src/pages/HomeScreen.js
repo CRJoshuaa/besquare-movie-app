073c12252486efdc009d0b2cf803024991559c80
aizadrzo <51921003+aizadrzo@users.noreply.github.com>
2021-10-20 10:49:36 +0800

    Branch aizad (#10)
    * my first commit
    
    * remove unused files
    
    * barebones featuremovie file
    
    * hi

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Categories from "../components/Categories";
 import FeatureMovie from "../components/FeatureMovie";
 import Header from "../components/Header";
 import "./HomeScreen.css";
+import "../App.css";
 
 function HomeScreen() {
   return (
