bce87523fbe2da0ed2d0363c138cb523bec6fdf3
Thisyahlen Nair <thisyahlen@besquare.com.my>
2021-10-20 10:39:01 +0800

    lol

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import HomeScreen from "./pages/HomeScreen";
+import FeatureMovie from "./components/FeatureMovie";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <HomeScreen />
+      <FeatureMovie />
     </div>
   );
 }
--- src/components/FeatureMovie.js
+++ src/components/FeatureMovie.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./FeatureMovie.css";
+import { Link } from "react-router-dom";
 
 function FeatureMovie() {
   const [movie, setMovie] = useState([]);
