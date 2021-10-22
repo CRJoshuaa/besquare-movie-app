6e7824c7564c46518485978c38524700babba292
Vishnu Janarthanan <vishnujanarthanan99@gmail.com>
2021-10-22 10:46:19 +0800

    centered icon footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -42,7 +42,7 @@ function Footer(props) {
                 <FontAwesomeIcon icon={faInstagram} size="2x" />
               </a>
             </li>
-            <li className="social-icon__item">
+            <li className="lastSocialIcon">
               <a
                 className="social-icon__link"
                 href="https://www.facebook.com/"
