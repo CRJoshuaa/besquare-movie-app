ec1f8aa8be83d4a895ec4fb204e16b01dce1bf06
Joshuaa <joshuaa.chales@gmail.com>
2021-10-22 10:43:15 +0800

    Edited

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route exact path="/">
-            <ProfilePage />
+            <HomeScreen />
           </Route>
           <Route path="/moviePage">
             <MoviePage />
