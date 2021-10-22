c38bf59d0187a9a27ea4aac900ab4016a0872e1f
Joshuaa <joshuaa.chales@gmail.com>
2021-10-22 15:59:48 +0800

    edited

--- src/components/Row.js
+++ src/components/Row.js
@@ -7,6 +7,7 @@ import CategoryPoster from "./CategoryPoster";
 function Row({ title, fetchURL, isLargeRow = false }) {
   const [movies, setMovies] = useState([]);
 
+  //fetch data
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchURL);
@@ -20,6 +21,7 @@ function Row({ title, fetchURL, isLargeRow = false }) {
     <div className="row">
       <h1 className="header">{title}</h1>
       <div className="row-panels">
+        {/* render movie panels */}
         {movies.map((movie) => {
           return <CategoryPoster key={movie.id} movieObj={movie} />;
         })}
